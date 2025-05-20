97f6b1b5b6291015683ebf3750fde63c3e4446ac
CTardencilla <calebtardencilla@gmail.com>
2025-05-20 11:33:25 -0600

    busca palabra

---
+++ 4.py
@@ -0,0 +1,7 @@
+palabras = ["manzana", "banana", "uva", "pera", "melón"]
+buscar = input("Ingrese una palabra para buscar: ")
+
+if buscar in palabras:
+    print("La palabra está en la lista.")
+else:
+    print("La palabra NO está en la lista.")
